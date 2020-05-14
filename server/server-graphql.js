06d58007c4fedaf8061b829c58b737b52e26a102
RosenUrkov <rosen.urkov@gmail.com>
2020-05-14 02:05:03 +0000

    Minor fixes

--- server/server-graphql.js
+++ server/server-graphql.js
@@ -1,6 +1,6 @@
-const { gql, ApolloServer } = require('apollo-server');
-const { v4: uuid } = require('uuid');
-const data = require('./data.json');
+const { gql, ApolloServer } = require("apollo-server");
+const { v4: uuid } = require("uuid");
+const data = require("./data.json");
 
 const typeDefs = gql`
   type Recipe {
@@ -38,20 +38,20 @@ const resolvers = {
   Query: {
     getRecipes() {
       return data.slice();
-    }
+    },
   },
   Mutation: {
     addRecipe(_, { recipe }) {
       const createdRecipe = {
         ...recipe,
         id: uuid(),
-        ingredients: recipe.ingredients.map(ing => ({ ...ing, id: uuid() }))
+        ingredients: recipe.ingredients.map((ing) => ({ ...ing, id: uuid() })),
       };
 
       data.push(createdRecipe);
       return createdRecipe;
-    }
-  }
+    },
+  },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
--- server/server-rest.js
+++ server/server-rest.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const { v4: uuid } = require('uuid');
-const data = require('./data.json');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { v4: uuid } = require("uuid");
+const data = require("./data.json");
 
 const app = express();
 app.use(cors(), bodyParser.json());
 
-app.get('/recipes', (req, res) => {
+app.get("/recipes", (req, res) => {
   res.send(data);
 });
 
-app.post('/recipes', (req, res) => {
+app.post("/recipes", (req, res) => {
   const recipe = req.body;
 
   const createdRecipe = {
     ...recipe,
     id: uuid(),
-    ingredients: recipe.ingredients.map(ing => ({ ...ing, id: uuid() }))
+    ingredients: recipe.ingredients.map((ing) => ({ ...ing, id: uuid() })),
   };
 
   data.push(createdRecipe);
   res.send(createdRecipe);
 });
 
-app.listen(4001, () => `Express magic happens at port 4001`);
+app.listen(4001, () => console.log(`Express magic happens at port 4001`));
